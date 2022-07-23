09c61d3bcd39d8d3610dc1cdf204695995fc6ec1
rich <1798435153@qq.com>
2022-07-24 01:18:26 +0800

    added done-true condition

--- brax/training/agents/apg2/train.py
+++ brax/training/agents/apg2/train.py
@@ -137,19 +137,19 @@ def train(environment: envs.Env,
       nstate = jax.lax.cond(
           jnp.mod(step_index + 1, truncation_length) == 0.,
           jax.lax.stop_gradient, lambda x: x, nstate)
-    '''
+  
     # ex_state_dones = jnp.expand_dims(env_state.done, -1)
     ex_dones = jnp.expand_dims(nstate.done, -1)
     # state_c, nstate_c = jnp.copy(env_state), jnp.copy(nstate)
     def cut_done_gradient(carry, target_t):
       state, next_state, next_state_done = target_t
       # state = jax.lax.cond(state_done.at[0].get(), jax.lax.stop_gradient, lambda x: x, next_state)
-      next_state = jax.lax.cond(next_state_done.at[0].get(), jax.lax.stop_gradient, lambda x: x, next_state)
+      next_state = jax.lax.cond(next_state_done.at[0].get() == True, jax.lax.stop_gradient, lambda x: x, next_state)
       return carry, (state, next_state)
     (_), (states, nstates) = jax.lax.scan(
       cut_done_gradient,
       (), (env_state, nstate, ex_dones), length = num_envs)
-    '''
+  
     return (nstate, key), (nstate.reward, env_state.obs, nstate.obs, state_extras)
 
   def data_generating(policy_params, normalizer_params, key):
