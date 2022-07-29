e979deda770521ea9992270884d3a515eeb3e814
rich <1798435153@qq.com>
2022-07-29 11:57:59 +0800

    added log info.

--- brax/training/agents/sac2/losses.py
+++ brax/training/agents/sac2/losses.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Soft Actor-Critic losses.
+"""
+Soft Actor-Critic losses.
 
 See: https://arxiv.org/pdf/1812.05905.pdf
 """
