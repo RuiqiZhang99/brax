dd4299854d15f43d85b473258ec4dd269ccec260
rich <1798435153@qq.com>
2022-08-03 00:45:39 +0800

    use the SNMLP(normalization)

--- brax/training/agents/sac2/networks.py
+++ brax/training/agents/sac2/networks.py
@@ -17,7 +17,7 @@
 from typing import Sequence, Tuple
 
 from brax.training.agents.sac2 import utils
-from brax.training import networks
+
 from brax.training import types
 from brax.training.types import PRNGKey
 import flax
@@ -26,8 +26,8 @@ from flax import linen
 
 @flax.struct.dataclass
 class SACNetworks:
-  policy_network: networks.FeedForwardNetwork
-  q_network: networks.FeedForwardNetwork
+  policy_network: utils.FeedForwardNetwork
+  q_network: utils.FeedForwardNetwork
   parametric_action_distribution: utils.ParametricDistribution
 
 
@@ -55,16 +55,16 @@ def make_sac_networks(
     preprocess_observations_fn: types.PreprocessObservationFn = types
     .identity_observation_preprocessor,
     hidden_layer_sizes: Sequence[int] = (256, 256),
-    activation: networks.ActivationFn = linen.relu) -> SACNetworks:
+    activation: utils.ActivationFn = linen.relu) -> SACNetworks:
   """Make SAC networks."""
   parametric_action_distribution = utils.NormalTanhDistribution(event_size=action_size)
-  policy_network = networks.make_policy_network(
+  policy_network = utils.make_policy_network(
       parametric_action_distribution.param_size,
       observation_size,
       preprocess_observations_fn=preprocess_observations_fn,
       hidden_layer_sizes=hidden_layer_sizes,
       activation=activation)
-  q_network = networks.make_q_network(
+  q_network = utils.make_q_network(
       observation_size,
       action_size,
       preprocess_observations_fn=preprocess_observations_fn,
